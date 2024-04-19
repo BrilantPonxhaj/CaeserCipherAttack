460e6db17cd2310f0e17753382c1fa9c352a557e
Brilant <brilant.ponxhaj@student.uni-pr.edu>
2024-04-19 22:34:47 +0200

    Funksioni Mesazhi i llogaritur për ta kthyer revers zhvendosjen e enkriptimit cezarit.

---
+++ SulmiCezarit.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SulmiCezarit {
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+    public static String Mesazhi(String Ciphertext, int celsi) {
+        StringBuilder tekstiDekoduar = new StringBuilder(Ciphertext);
+        for (int i = 0; i < Ciphertext.length(); i++) {
+            char ch = Ciphertext.charAt(i);
+            if (ch >= 'A' && ch <= 'Z')
+                if (Ciphertext.charAt(i) != ' ')
+                    tekstiDekoduar.setCharAt(i, (char) (((Ciphertext.charAt(i) - 'A' - celsi + 26) % 26) + 'A'));
+        }
+
+        return tekstiDekoduar.toString();
+    }
+}
